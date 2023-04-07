48bf30ba26c660ceaea1e0f2e871e26f53a3ed80
Abhijeet Singh Malhi <amalhi@andrew.cmu.edu>
2023-04-07 15:12:02 -0400

    New scan implementation
    - Implemented new scan functionality

---
+++ Assets/Scripts/scanNew.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scanNew : MonoBehaviour
+{
+    public Camera playerCamera;
+    public Material originalSkybox;
+    public Material scanSkybox;
+    private bool scannerActive = false;
+    int oldMask;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        oldMask = playerCamera.cullingMask;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            print("S pressed");
+            if (scannerActive)
+            {
+                //Activate Culling mask
+                int scanLayer = 1 << 10;
+                playerCamera.cullingMask = scanLayer;
+                RenderSettings.skybox = scanSkybox;
+            }
+            else
+            {
+                //Deactivate Culling mask
+                playerCamera.cullingMask = oldMask;
+                RenderSettings.skybox = originalSkybox;
+            }
+            scannerActive = !scannerActive;
+        }
+    }
+}
