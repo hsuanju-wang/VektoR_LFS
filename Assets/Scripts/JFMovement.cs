55451cb0f6e9a35877133ee59a66ce4c569f1e45
Abhijeet Singh Malhi <amalhi@andrew.cmu.edu>
2023-04-21 17:07:45 -0400

    Jelly fish movement updated
    - Updated Jellyfish Movement to cap the ceiling and bottom point

--- Assets/Scripts/JFMovement.cs
+++ Assets/Scripts/JFMovement.cs
@@ -8,12 +8,15 @@ public class JFMovement : MonoBehaviour
     public float m_ThrustX = 1f;
     public float m_ThrustY = 1f;
     private bool slugMoving = true;
+    public float maxY = 10f;
+    public float minY = 5f;
+    bool maxCrossed = false;
     // Start is called before the first frame update
     void Start()
     {
         Random.InitState((int)this.name.GetHashCode());
         m_Rigidbody = GetComponent<Rigidbody>();
-        m_ThrustX = Random.Range(-0.5f, 0.5f);
+        m_ThrustX = Random.Range(-0.3f, 0.3f);
         m_ThrustY = Random.Range(0.3f, 0.7f);
     }
 
@@ -22,13 +25,30 @@ public class JFMovement : MonoBehaviour
     {
         if(slugMoving){
             StartCoroutine(SlugMove());
+            if (transform.position.y > maxY)
+            {
+                maxCrossed = true;
+            }
+            if(transform.position.y < minY)
+            {
+                maxCrossed = false;
+            }
         }
     }
 
     private IEnumerator SlugMove()
     {
         slugMoving = false;
-        m_Rigidbody.AddForce( m_ThrustX, m_ThrustY, 0, ForceMode.Impulse);
+        if (maxCrossed)
+        {
+            //Go Down
+            m_Rigidbody.AddForce(m_ThrustX, m_ThrustY*-1, 0, ForceMode.Impulse);
+        } else
+        {
+            //Go Up till maxCrossed
+            m_Rigidbody.AddForce(m_ThrustX, m_ThrustY, 0, ForceMode.Impulse);
+        }
+        
         yield return new WaitForSeconds(2f);
         slugMoving = true;
     }
